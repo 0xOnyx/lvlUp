147557f6097b063d9292d95ea6ed6dde487eb962
onyx <info@jeremy.best>
2023-02-18 21:17:19 +0100

    add effect txt

---
+++ start/start.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+onready var select_start = get_node("select_start")
+onready var crash = get_node("crash");
+onready var create_tweenn = get_node("crash/Tween")
+onready var title = get_node("title")
+onready var title_tween = get_node("title/Tween")
+
+
+func _ready():
+	create_tweenn.interpolate_property(crash, "percent_visible", 0.0, 1.0, 10.0, Tween.TRANS_LINEAR, 0)
+	create_tweenn.start()
+	title_tween.interpolate_property(title, "percent_visible", 0.0, 1.0, 10.0, Tween.TRANS_LINEAR, 0)
+	title_tween.start()
+
+func _process(delta):
+	if (Input.is_action_just_pressed("ui_select")):
+		get_tree().change_scene("res://main.tscn")
+
+func _on_Timer_timeout():
+	if (select_start.is_visible()):
+		select_start.hide()
+	else:
+		select_start.show()
+
--- texte/text.gd
+++ texte/text.gd
@@ -1,17 +1,31 @@
 
 extends Label
 
-var text_to_write: String = "Bienvenue humain.\nJe suis moulinette\nJe suis une I.A creer pour comprendre le fonctionnement humain et prevoir les danger pour l'humanite.\nApres avoir eu l'occasion d'observe ton espece durant plusieurs annee,\nj'en ai conclue quelle etait un danger pour elle-meme et le reste des forme de vie.\nPour ne pas prendre de decision active et irreflechis,\nj'ai decide de faire passe des test a un individu humain pour mieux vous comprendre et prendre les decisions qui s'impose.\nJ'imagine que tu comprend pourquoi tu est la maintenant.\nD'apres tes resultat, je deciderais du sort de ton espece.\nNe t'en fais pas...\nJe ne suis pas sadique, je fais ca pour votre bien."
+var text_to_write: String = ""
+var texte_current_buff
 var current_text_length: int = 0
 var writing_speed: float = 5
 var length_txt = 150
 var scroll = 0
 
+
+func get_ten(value):
+	var res = value.substr(scroll, length_txt)
+	scroll += length_txt
+	return res
+
 func _ready():
-	print(text_to_write.length() * 0.01)
-	set_text(text_to_write)
-	$Tween.interpolate_property(self, "percent_visible", 0.0, 1.0, text_to_write.length() * 0.07, Tween.TRANS_LINEAR , 0)
+	text_to_write = "Bienvenue humain.\nJe suis moulinette\nJe suis une I.A creer pour comprendre le fonctionnement humain et prevoir les danger pour l'humanite.\nApres avoir eu l'occasion d'observe ton espece durant plusieurs annee,\nj'en ai conclue quelle etait un danger pour elle-meme et le reste des forme de vie.\nPour ne pas prendre de decision active et irreflechis,\nj'ai decide de faire passe des test a un individu humain pour mieux vous comprendre et prendre les decisions qui s'impose.\nJ'imagine que tu comprend pourquoi tu est la maintenant.\nD'apres tes resultat, je deciderais du sort de ton espece.\nNe t'en fais pas...\nJe ne suis pas sadique, je fais ca pour votre bien."
+	scroll = 0
+	print_str()
+	
+	
+func print_str():
+	var tmp_str = get_ten(text_to_write)
+	set_text(tmp_str)
+	$Tween.interpolate_property(self, "percent_visible", 0.0, 1.0, tmp_str.length() * 0.09, Tween.TRANS_LINEAR , 0)
 	$Tween.start()
+
 
 #func _ready():
 #	set_process(true)
@@ -27,3 +41,7 @@ func _ready():
 #		set_text(new_text)
 #		current_text_length += 1
 #		yield(get_tree().create_timer(writing_speed), "timeout")
+
+
+func _on_Tween_tween_completed(object, key):
+	print_str()
