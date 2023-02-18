753f077b81afba9a1160d2fced7b025e0ee822ef
znichola <niki.zivkovic@hotmail.com>
2023-02-18 13:10:34 +0100

    tmp

--- scenes/level_1/level_1.gd
+++ scenes/level_1/level_1.gd
@@ -1,6 +1,26 @@
 extends Node2D
 
+
+var simultaneous_scene = preload("res://scenes/level_2/level_2.tscn").instance()
+
+var free_me = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _process(_delta):
+	if free_me:
+		_add_a_scene_manually()
+
+
+func _on_Door_area_entered(_area):
+#	get_tree().change_scene("res://scenes/level_2/level_2.tscn")
+	print("we collected a ...")
+	free_me = true
+
+func _add_a_scene_manually():
+	# This is like autoloading the scene, only
+	# it happens after already loading the main scene.
+	get_parent().add_child(simultaneous_scene)
+	call_deferred("queue_free")
