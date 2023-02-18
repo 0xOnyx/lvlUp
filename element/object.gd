ebaf30b154612566b09ac15ebd7850fc22a2045b
znichola <niki.zivkovic@hotmail.com>
2023-02-18 06:57:36 +0100

    sleep

--- element/object.gd
+++ element/object.gd
@@ -1,32 +1,29 @@
-extends KinematicBody2D
+extends Area2D
 
 const SPEED = 10
-export var inputmap = ["action_1"]
 var velocity = Vector2.ZERO
 var following = false
-var target: Node2D
+var target: KinematicBody2D = null
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	velocity = Vector2.ZERO
 	
 	if following and target:
 		position = target.position
 
-
 func _input(event):
-	if event.is_action_pressed(inputmap[0]) and target:
-		print(following)
-		print(position.distance_to(target.position))
+	if target and event.is_action_pressed("action_"+String(target.playernum)):
 		if following:
 			following = false
 			target.position = position
 			target = null
-		elif  target and position.distance_to(target.position) < 400:
+		elif  target:
 			following = true
 			
 
-func _on_Follower_body_entered(body):
-	target = body
+func _on_Area2D_body_entered(body):
+	if body.get_class() == "KinematicBody2D":
+		target = body
 
-func _on_Follower_body_exited(body):
-	pass
+func _on_Area2D_body_exited(_body):
+	target = null
