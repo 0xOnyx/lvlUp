30e16d5b29bb1a92fcc48808173c75b2fd5cc4c8
onyx <info@jeremy.best>
2023-02-19 10:21:58 +0100

    fix on production

--- start/start.gd
+++ start/start.gd
@@ -6,11 +6,6 @@ onready var create_tweenn = get_node("crash/Tween")
 onready var title = get_node("title")
 onready var title_tween = get_node("title/Tween")
 
-var t = TTS.new()
-var script = "Hello world. This is a test!"
-var is_spoken = t.say_text(script)
-print('is_spoken: ', is_spoken)
-
 func _ready():
 	create_tweenn.interpolate_property(crash, "percent_visible", 0.0, 1.0, 10.0, Tween.TRANS_LINEAR, 0)
 	create_tweenn.start()
